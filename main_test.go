e2321ba8a3ffdd3673648cc7d9aedc6bcb841b95
rodrigo <rodrigo@rodrigos-MacBook-Pro.local>
2015-10-21 14:59:12 -0400

    testing

--- main_test.go
+++ main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestAddSpace(t *testing.T) {
+	originalString := "ClientLoveProcess"
+	spacedString := "Client Love Process"
+
+	testString := addSpace(originalString)
+
+	if spacedString != testString {
+		t.Error("String not spaced")
+	}
+}
